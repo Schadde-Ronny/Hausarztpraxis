5fc26b469607684ac611f90e78fb66f28ffa2212
Schadde-Ronny <frevert71@gmail.com>
2023-10-16 20:01:36 +0200

    Seite für Termine fertiggestellt

---
+++ js/leistungsspektrum.js
@@ -0,0 +1,71 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleSwitch = document.getElementById("toggle-switch");
+
+    toggleSwitch.addEventListener("change", function () {
+        if (toggleSwitch.checked) {
+            // Aktiviert: Dark Mode einschalten
+            enableDarkMode();
+        } else {
+            // Deaktiviert: Dark Mode ausschalten
+            disableDarkMode();
+        }
+    });
+
+    // Überprüfen, ob der Dark Mode gespeichert ist
+    if (localStorage.getItem("darkModeEnabled") === "true") {
+        enableDarkMode();
+        toggleSwitch.checked = true;
+    }
+});
+
+function enableDarkMode() {
+    // Dark Mode aktivieren
+    document.body.classList.add("dark-mode");
+    // Den Dark-Modus-Status speichern
+    localStorage.setItem("darkModeEnabled", "true");
+}
+
+function disableDarkMode() {
+    // Dark Mode deaktivieren
+    document.body.classList.remove("dark-mode");
+    // Den Dark-Modus-Status speichern
+    localStorage.setItem("darkModeEnabled", "false");
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleSwitch = document.getElementById("toggle-switch");
+
+    toggleSwitch.addEventListener("change", function () {
+        if (toggleSwitch.checked) {
+            // Aktiviert: Dark Mode einschalten
+            enableDarkMode();
+        } else {
+            // Deaktiviert: Dark Mode ausschalten
+            disableDarkMode();
+        }
+    });
+
+    // Überprüfen, ob der Dark Mode gespeichert ist
+    if (localStorage.getItem("darkModeEnabled") === "true") {
+        enableDarkMode();
+        toggleSwitch.checked = true;
+    }
+});
+
+function enableDarkMode() {
+    // Dark Mode aktivieren
+    document.body.classList.add("dark-mode");
+    // Den Dark-Modus-Status speichern
+    localStorage.setItem("darkModeEnabled", "true");
+}
+
+function disableDarkMode() {
+    // Dark Mode deaktivieren
+    document.body.classList.remove("dark-mode");
+    // Den Dark-Modus-Status speichern
+    localStorage.setItem("darkModeEnabled", "false");
+}
+
+
+
